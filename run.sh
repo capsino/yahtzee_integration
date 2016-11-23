ef950939c7839ccae97c8fad87d30240dba647f9
James Relph <james.relph@capgemini.com>
2016-11-23 15:00:38 +0000

    Finished testing

--- run.sh
+++ run.sh
@@ -5,5 +5,3 @@ server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}
 
 curl --include --header "Accept: application/json" http://${client_ip}:80
 echo
-curl --include --header "Accept: application/json" http://${server_ip}:4567
-echo

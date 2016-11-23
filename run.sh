35e0d1be6296a73cce3d39b35ee428744126628d
James Relph <james.relph@capgemini.com>
2016-11-23 15:19:47 +0000

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 client_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yahtzee_client)
-server_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yahtzee_server)
 
 curl --include --header "Accept: application/json" http://${client_ip}:80
 echo
